f9f9487ed786480545d342881dc0dec30c65620c
Fajeralk <66381900+Fajeralk@users.noreply.github.com>
2020-07-25 17:07:47 -0700

    added forecast code

--- script.js
+++ script.js
@@ -72,6 +72,35 @@ function displayWeatherCondition(response) {
     response.data.main.temp
   );
 }
+
+unction dispalyForecast(response) {
+  let forecastElement = document.querySelector("#forecast-hours");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+      forecast.weather[0].icon
+      }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
+}
+
 function searchCity(city) {
   let apiKey = "3845bbf755c7a6b2d8df3dba924feec5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
